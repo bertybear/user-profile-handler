e747ed23b455d6296f085b65ca8e387fc5e93fef
Eesa Jacobs <eesa@allaboutapps.co.za>
2024-12-07 18:16:34 +0200

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -20,17 +20,10 @@ def get_user_profile():
         decoded_token = jwt.decode(auth_token, options={"verify_signature": False})
         username = decoded_token.get("username")
 
-        print(repository.find_by_cognito_username(username))
+        user_profile = repository.find_by_cognito_username(username)
+        del user_profile['cognito_username']
 
-        # {'sub': 'f6d2e224-6051-7084-c01c-fe5a222971d4', 'iss': 'https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_QBpffJzGv', 'client_id': '1c3jdq0r74jhlmkrsnjsgpmhgg', 'origin_jti': '83ebcc59-2c76-4665-8435-031452c8d8cc', 'event_id': 'f6c66a4f-4a61-43b2-809d-37d6aec4a8cd', 'token_use': 'access', 'scope': 'aws.cognito.signin.user.admin', 'auth_time': 1730100200, 'exp': 1730657212, 'iat': 1730655412, 'jti': '131eda4e-d5c5-446d-833c-0638598cd4a5', 'username': 'f6d2e224-6051-7084-c01c-fe5a222971d4'}
-
-        # Get the Identity Pool ID
-        identity_pool_id = ""
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"identity_pool_id": identity_pool_id}),
-        }
+        return user_profile
     except Exception as e:
         logger.error(f"Error retrieving identity pool ID: {str(e)}")
         return {"statusCode": 500, "body": "Internal Server Error"}
