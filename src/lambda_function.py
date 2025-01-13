e5fbcd23cd3ca5363109495a838ab3974d4cd50b
Eesa Jacobs <eesa@allaboutapps.co.za>
2025-01-13 21:33:13 +0200

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,7 @@
 import json
 import os
+
+# from aws_lambda_powertools import Logger
 from config.amazon_factory import AmazonSqsFactory
 from events.apigateway_event_handler import apigateway_event_handler
 from events.sns_event_handler import sns_event_handler
@@ -13,4 +15,7 @@ def lambda_handler(event, context):
     except Exception as exception:
         dead_letter_queue = AmazonSqsFactory().Queue(os.getenv('DEAD_LETTER_QUEUE_URL'))
         dead_letter_queue.send_message(MessageBody=json.dumps(event))
-        return { "message": "Internal Server Error" }, 500
+        
+        # Logger().exception(exception)
+    
+        raise exception
