13257064f0cbbdef60ef896d6ad3d9136098f999
Eesa Jacobs <eesa@allaboutapps.co.za>
2025-09-23 00:41:27 +0200

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,10 +4,11 @@ from events.apigateway_event_handler import apigateway_event_handler
 from events.sns_event_handler import sns_event_handler
 
 def lambda_handler(event, context):
+
     if 'routeKey' in event:
         return apigateway_event_handler(event, context)
     elif 'Records' in event:
-        source = event['Records'][0].get('eventSource')
+        source = event['Records'][0].get('eventSource') or event['Records'][0].get('EventSource')
         
         if source == 'aws:sns':
             return sns_event_handler(event, context)
